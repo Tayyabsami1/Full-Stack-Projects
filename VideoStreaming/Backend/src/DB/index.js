86425a88c9e5d7acc2166f0d51ae8c25e50c14e6
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2024-07-06 16:39:41 +0500

    Proffessional Backend Setup and MongoDB connection

---
+++ VideoStreaming/Backend/src/DB/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_Name } from "../constants.js"
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`);
+        console.log("Database Connected Success ", `Host ${connectionInstance.connection.host}`);
+    }
+    catch (err) {
+        console.log("Database connection error: ", err);
+    }
+}
+
+export default connectDB;
---
+++ VideoStreaming/Backend/src/index.js
@@ -0,0 +1,5 @@
+import dotenv from "dotenv/config";
+import connectDB from "./DB/index.js";
+
+
+connectDB();
