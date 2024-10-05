35932cdc0d5b5d84bf25eda0ff1715c78195397e
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2024-10-05 18:44:27 +0500

    Useless project removed

--- c-anonymous-messenger/MyNotes.md
+++
@@ -1,3 +0,0 @@
-# NextJS Notes 
-- Models is the folder in which we actually define the schema of our application
-- We make another folder of Schema , in this we do some verifications advanced one using libraries such as ZOD
--- c-anonymous-messenger/src/schemas/messageSchema.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-
-export const messageSchema = z.object({
-    content: z.string().min(10, { message: "Content must atleast contain 10 characters" }).max(300, { message: "Content must not cross 300 characters" }),
-})
